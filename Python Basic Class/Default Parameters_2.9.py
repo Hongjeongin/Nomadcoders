834057f44584ebffd4b0707543d37ef8ffe83304
Hongjeongin <64721629+Hongjeongin@users.noreply.github.com>
2022-10-24 20:05:20 +0900

    Create Repos and make calcul functions

---
+++ Python Basic Class/Default Parameters_2.9.py
@@ -0,0 +1,22 @@
+def plus(a = 0, b = 0):
+    print(a + b)
+def minus(a = 0, b = 0):
+    print(a - b)
+def multi(a = 0, b = 0):
+    print(a * b)
+def divis(a = 0, b = 1):
+    print(a / b)
+def double(a = 0, b = 0):
+    print(a ** b)
+
+plus(3, 5)
+minus(5, 2)
+multi(10, 3)
+divis(10, 5)
+double(2, 10)
+print("-------------------------")
+plus()
+minus()
+multi()
+divis()
+double()
